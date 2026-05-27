bd4ad7f727fec793aa528d1bd62dddb5e4ee88eb
roundsalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-05-27 17:40:35 -0500

    Fix review screen compilation

--- app/src/main/kotlin/dev/soupslurpr/appverifier/ui/ReviewPrivacyPolicyAndLicense.kt
+++ app/src/main/kotlin/dev/soupslurpr/appverifier/ui/ReviewPrivacyPolicyAndLicense.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
