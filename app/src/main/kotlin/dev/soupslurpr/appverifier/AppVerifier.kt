be97a67be5149be63e6466a93ca9ec6773e3f26c
roundsalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-05-31 13:09:29 -0500

    Compare all hashes as unordered sets regardless of signing scheme

--- app/src/main/kotlin/dev/soupslurpr/appverifier/AppVerifier.kt
+++ app/src/main/kotlin/dev/soupslurpr/appverifier/AppVerifier.kt
@@ -253,8 +253,7 @@ fun AppVerifierApp(
 
                 val sharedTextEntryForVerify = filteredEntries?.find { it.packageName == currentPackageName }
                 val sharedTextHashMatchForVerify = if (sharedTextEntryForVerify != null && sharedTextEntryForVerify.hashes.isNotEmpty()) {
-                    if (currentHashes.hasMultipleSigners) sharedTextEntryForVerify.hashes.toSet() == currentHashes.hashes.toSet()
-                    else currentHashes.hashes.last() in sharedTextEntryForVerify.hashes
+                    sharedTextEntryForVerify.hashes.toSet() == currentHashes.hashes.toSet()
                 } else {
                     null
                 }
@@ -281,8 +280,7 @@ fun AppVerifierApp(
                     DatabaseStatusDisplayMode.valueOf(preferencesUiState.value.databaseStatusDisplayMode.second.value),
                     userDatabaseEntries.find { it.packageName == currentPackageName },
                     userDatabaseEntries.find { it.packageName == currentPackageName }?.let { entry ->
-                        if (currentHashes.hasMultipleSigners) entry.hashes.toSet() == currentHashes.hashes.toSet()
-                        else currentHashes.hashes.last() in entry.hashes
+                        entry.hashes.toSet() == currentHashes.hashes.toSet()
                     } ?: false,
                     {
                         snackbarCoroutineScope.launch {
