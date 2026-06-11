7492a760e0385921fb896372fc9cdaea83fd97b8
roundsalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-06-11 14:49:24 -0500

    Bump version to 0.5.0

--- gradle/version.gradle.kts
+++ gradle/version.gradle.kts
@@ -1,4 +1,4 @@
 extra.set("appVersionMajor", 0)
-extra.set("appVersionMinor", 4)
+extra.set("appVersionMinor", 5)
 extra.set("appVersionPatch", 0)
-extra.set("appVersionCode", 16)
+extra.set("appVersionCode", 17)
