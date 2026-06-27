47e46e461b916bc12d3b93170a8ec5edf3a467b9
roundsalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-06-27 00:56:19 +0000

    Sync privacyguides/verified-apps database

--- app/src/main/kotlin/dev/soupslurpr/appverifier/InternalVerificationInfoDatabase.kt
+++ app/src/main/kotlin/dev/soupslurpr/appverifier/InternalVerificationInfoDatabase.kt
@@ -15841,6 +15841,7 @@ val internalVerificationInfoDatabase = setOf(
 
 
 
+
 val internalVerificationInfoDatabaseMap: Map<String, InternalDatabaseVerificationInfo> by lazy {
     internalVerificationInfoDatabase.associateBy { it.packageName }
 }
