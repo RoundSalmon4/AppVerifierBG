fcc41b605ea5c7e90c4eb3b1e2b47fa9b28e036b
RoundSalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-07-14 13:00:01 -0500

    Bump version to 0.6.2 (versionCode 26)

--- gradle/version.gradle.kts
+++ gradle/version.gradle.kts
@@ -1,4 +1,4 @@
 extra.set("appVersionMajor", 0)
 extra.set("appVersionMinor", 6)
-extra.set("appVersionPatch", 1)
-extra.set("appVersionCode", 25)
+extra.set("appVersionPatch", 2)
+extra.set("appVersionCode", 26)
