09bb37923efd46b72a571bf7e9e9c8e9c086217b
RoundSalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-07-16 09:33:29 -0500

    Fix missing clip and Color imports in SettingsScreen

--- app/src/main/kotlin/dev/soupslurpr/appverifier/ui/SettingsScreen.kt
+++ app/src/main/kotlin/dev/soupslurpr/appverifier/ui/SettingsScreen.kt
@@ -55,6 +55,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
